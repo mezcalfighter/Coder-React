35ac0bf1f610475f8fdc72c1aecaa7bb9892fe0f
Emanuel Camarena <emanuel.ortega@hotmail.com>
2022-12-16 00:12:52 -0600

    Added nivelacion, missing async & await

---
+++ Nivelacion/Minimos JS/newApp.js
@@ -0,0 +1,15 @@
+//Modules
+export function newAdd(x,y){
+    return x + y;
+}
+
+export function newMultiply(x,y){
+    return x * y;
+}
+
+export const title = "JS for React";
+export const active = true;
+export const points = [10,20,30];
+
+//Export by default
+export default title;
